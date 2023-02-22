67e794fddd35ea48477627943792e011860da559
lebrancconvas <lebrancconvas@gmail.com>
2023-02-22 18:42:06 +0700

    ✨ feat: update

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import (
-	
-)
+import "fmt"
+
+type Person struct {
+	FirstName string
+	LastName string
+}
+
+func (p Person) shout() {
+	fmt.Printf("%v %v !!\n", p.FirstName, p.LastName)
+}
 
 func main() {
-	
+	p := Person{"Jeng", "Mahapak"}
+	p.shout()
 }
